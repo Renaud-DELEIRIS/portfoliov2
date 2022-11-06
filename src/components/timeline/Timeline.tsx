13e733e31d6a7cc7f9ffb775ebfde7060f504a5d
renaud <renaud@ganister.eu>
2022-11-06 14:42:36 -0500

    timeline responsive

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -3,25 +3,25 @@ import React from "react";
 import works from "src/constant/work";
 
 // Create a function that generatore cold to warm color (blue to red) based on the number of works
-const generateColor = (index: number) => {
+const generateColor = (index: number, saturate = true) => {
   const color = Math.floor(((works.length - index) / works.length) * 350);
-  return `hsl(${color}, 100%, 50%)`;
+  return `hsl(${color}, ${saturate ? "70" : "40"}%, 50%)`;
 };
 
 export default function Timeline() {
   const { t } = useTranslation("timeline");
   return (
     <div className="w-full row relative items-center">
-      <div className="px-4 w-full row min-h-[450px] ">
+      <div className="p-4 w-full col md:row md:min-h-[450px] gap-8 md:gap-0">
         {works.map((work, index) => (
           <div
             key={index}
-            className={`col items-center flex-1 ${
-              index % 2 ? "flex-col-reverse" : ""
+            className={`row md:col md:items-center md:justify-start md:flex-1 ${
+              index % 2 ? "flex-row-reverse md:flex-col-reverse" : ""
             }`}
           >
             <div
-              className={`col justify-evenly items-center h-1/2 ${
+              className={`hidden md:flex col justify-evenly items-center w-1/2 md:h-1/2 ${
                 index % 2 ? "flex-col-reverse" : ""
               }`}
             >
@@ -33,7 +33,7 @@ export default function Timeline() {
               </h6>
             </div>
             <div
-              className={`col h-1/2 w-full gap-4  ${
+              className={`hidden md:flex col w-1/2 md:h-1/2 md:w-full gap-4  ${
                 index % 2 ? "flex-col-reverse" : ""
               }`}
             >
@@ -41,7 +41,7 @@ export default function Timeline() {
                 className={`text-neutral-100 dark:text-neutral-800 font-bold capitalize text-center w-full h-12 border ${
                   index % 2 ? "rounded-t-full" : "rounded-b-full"
                 } dark:border-neutral-800 flex items-center justify-center min-h-[48px]`}
-                style={{ backgroundColor: generateColor(index) }}
+                style={{ backgroundColor: generateColor(index, false) }}
               >
                 {t(work.title)}
               </h6>
@@ -49,12 +49,34 @@ export default function Timeline() {
                 {t(work.description)}
               </p>
             </div>
+            <div
+              className={`px-2 row w-1/2 md:hidden items-center ${
+                index % 2 ? "" : "flex-row-reverse"
+              }`}
+            >
+              <div
+                className="p-2 col gap-1"
+                style={{
+                  backgroundColor: generateColor(index),
+                }}
+              >
+                <h6 className="font-bold text-color-1">{t(work.title)}</h6>
+                <h6 className="font-bold text-color-1 text-sm">
+                  {t(work.date)}
+                </h6>
+                <p>
+                  <span className="text-xs text-color-1">
+                    {t(work.description)}
+                  </span>
+                </p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
-      <div className="absolute w-full flex items-center">
+      <div className="absolute pointer-events-none h-full w-full md:row col items-center justify-center">
         <div className="h-4 w-4 rounded-full border-2 bg-neutral-1 border-neutral-700 dark:border-neutral-200"></div>
-        <div className="flex-1 h-1 bg-neutral-700"></div>
+        <div className="flex-1 w-1 md:h-1 md:w-full bg-neutral-700"></div>
         <div className="h-4 w-4 rounded-full border-2 bg-neutral-1 border-neutral-700 dark:border-neutral-200"></div>
       </div>
     </div>
